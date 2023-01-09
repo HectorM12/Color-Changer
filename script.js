8785f82d866f00a1692671937c3718d651372639
HectorM5 <90624620+HectorM12@users.noreply.github.com>
2023-01-09 14:47:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("red").addEventListener("mouseover", showRed);
+
+function showRed() {
+  document.getElementById("product-photo").src = "img/Red@2x.jpg";
+}
+
+document.getElementById("black").addEventListener("mouseover", showBlack);
+
+function showBlack() {
+  document.getElementById("product-photo").src = "img/Black@2x.jpg";
+}
+
+document.getElementById("lightblue").addEventListener("mouseover", showLightBlue);
+
+function showLightBlue() {
+  document.getElementById("product-photo").src = "img/Light Blue@2x.jpg";
+}
